6ad0c42b05d88cfc43deaf7b43996376abc98f7f
AanandBhandari <aanandbhandari143@gmail.com>
2019-05-21 07:45:07 +0545

    make mouse as reusable

--- src/components/RenderProps.js
+++ src/components/RenderProps.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
-export default class MouseTracker extends React.Component {
+// making mouse as reusable component
+class Mouse extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.handleMouseMove = this.handleMouseMove.bind(this)
-        this.state = {x:0,y:0}
+        this.state = {
+            x:0,
+            y:0
+        }
     }
     handleMouseMove(e) {
         this.setState({
@@ -15,8 +18,18 @@ export default class MouseTracker extends React.Component {
     render() {
         return (
             <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
-                <h1>Move the mouse around!</h1>
                 <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+            </div>
+        )
+    }
+}
+
+export default class MouseTracker extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Move the mouse around!</h1>
+                <Mouse />
             </div>
         )
     }
