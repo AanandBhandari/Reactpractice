d259b734c674ce634c2264f1df2385afbdd1a0fd
AanandBhandari <aanandbhandari143@gmail.com>
2019-07-20 09:10:14 +0545

    changed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Trello from './components/Trello';
 import Data from './data/testData';
 import LifeCycleDemo from './components/lifecycle';
-import AuthExample from './components/auth'
+// import AuthExample from './components/auth'
 import MouseTracker from './components/RenderProps'
 import Search from './components/search'
 // ReactDOM.render(<Trello data={Data}/>, document.getElementById('root'));
