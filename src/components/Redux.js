288ac32bfac9e2bcc3c364be60ed7b644cbc0fad
AanandBhandari <aanandbhandari143@gmail.com>
2019-08-19 17:45:37 +0545

    refactoring

--- src/components/Redux.js
+++ src/components/Redux.js
@@ -1,20 +1,76 @@
 import React from 'react';
 import {createStore} from 'redux';
 
+// action generator are functions that return action objects
+// const incrementCount = ({ incrementBy})=> {
+//     return {
+//         type: 'INCREMENT',
+//         // costum action
+//         incrementBy
+//     }
+// }
+
+// instead of using incrementBy params we make payload with default empty obj
+//  as sometime we may not send incrementBy to the genetrator the incrementBy wii be undefined
+// const incrementCount = (payload={})=> {
+//     return {
+//         type: 'INCREMENT',
+//         // costum action
+//         incrementBy : typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+//     }
+// }
+
+// by destructing the payload
+// const incrementCount = ({incrementBy} = {}) => {
+//     return {
+//         type: 'INCREMENT',
+//         // costum action
+//         incrementBy: typeof incrementBy === 'number' ? incrementBy : 1
+//     }
+// }
+
+// more simplified
+const incrementCount = ({incrementBy = 1} = {}) => {
+    return {
+        type: 'INCREMENT',
+        // costum action
+        incrementBy
+    }
+}
+
+const decrementCount = ({ decrementBy = 1 } = {}) => {
+    return {
+        type: 'DECREMENT',
+        // costum action
+        decrementBy
+    }
+}
+
+const resetCount = () => {
+    return {
+        type: 'RESET',
+    }
+}
+const setCount = ({set}) => {
+    return {
+        type: 'SET',
+        set
+    }
+}
+
+
 const Redux = () => {
     // createStore takes 2 params first the state with init value and 
     // anoter is action obj to take action according to action.type
     const store = createStore((state = {count:0},action)=>{
         switch (action.type) {
             case 'INCREMENT':
-                const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
                 return {
-                    count : state.count + incrementBy
+                    count : state.count + action.incrementBy
                 }
             case 'DECREMENT':
-                const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
                 return {
-                    count: state.count - decrementBy
+                    count: state.count - action.decrementBy
                 }
             case 'SET':
                 return {
@@ -37,22 +93,11 @@ const Redux = () => {
 
     // dispatch method takes action object to communicate with store,
     // action determines what kind of action to b taken
-    store.dispatch({
-        type: 'INCREMENT',
-        // costum action
-        incrementBy : 5
-    });
-    store.dispatch({
-        type: 'DECREMENT',
-        decrementBy : 3
-    })
-    store.dispatch({
-        type: 'SET',
-        set : 2
-    })
-    store.dispatch({
-        type: 'RESET'
-    })
+    store.dispatch(incrementCount({ incrementBy : 5}));
+    store.dispatch(decrementCount({ decrementBy : 10}));
+    store.dispatch(decrementCount({}))
+    store.dispatch(resetCount());
+    store.dispatch(setCount({set : 2}));
     return(
         <div>
 
