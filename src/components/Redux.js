03643500f33587a55cb868351726d6fbef729ebe
AanandBhandari <aanandbhandari143@gmail.com>
2019-08-19 16:55:27 +0545

    switch case and subscribe

--- src/components/Redux.js
+++ src/components/Redux.js
@@ -5,15 +5,49 @@ const Redux = () => {
     // createStore takes 2 params first the state with init value and 
     // anoter is action obj to take action according to action.type
     const store = createStore((state = {count:0},action)=>{
-        console.log(state);
-        return state;
+        switch (action.type) {
+            case 'INCREMENT':
+                return {
+                    count : state.count +1
+                }
+            case 'DECREMENT':
+                return {
+                    count: state.count - 1
+                }
+            case 'SET':
+                return {
+                    count: action.set
+                }
+            case 'RESET':
+                return {
+                    count: 0
+                }
+        
+            default:
+                return state;
+        }
     });
+    // suscribe will notify if state is changed
+    // it takes a function as params
+    store.subscribe(()=>{
+        console.log(store.getState());
+    })
+
     // dispatch method takes action object to communicate with store,
     // action determines what kind of action to b taken
     store.dispatch({
         type: 'INCREMENT'
+    });
+    store.dispatch({
+        type: 'DECREMENT'
     })
-    console.log(store.getState());
+    store.dispatch({
+        type: 'SET',
+        set : 2
+    })
+    store.dispatch({
+        type: 'RESET'
+    })
     return(
         <div>
 
